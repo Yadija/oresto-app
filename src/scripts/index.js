657532df8ad1e62b2bf72b5c7a3af8cc09157080
Yadija <ayuliadi01@gmail.com>
2022-11-15 21:59:17 +0800

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,40 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.scss';
+import '../styles/skip-content.scss';
+import '../styles/responsive.scss';
+import data from '../DATA.json';
+
+const posts = document.getElementById("posts");
+data.restaurants.forEach(restaurant => {
+  const card = document.createElement("div");
+  card.innerHTML = `
+    <div class="card tabindex="0"">
+      <div class="card-head">
+        <p>Kota ${restaurant.city}</p>
+        <img src="${restaurant.pictureId}" alt="${restaurant.name}">
+      </div>
+      <div class="card-desc">
+        <p class="rating">rating: ${restaurant.rating}</p>
+        <h3 class="title">${restaurant.name}</h3>
+        <p class="desc">${restaurant.description}</p>
+      </div>
+    </div>
+  `;
+
+  posts.appendChild(card);
+});
+
+// off canvas
+const hamburgerButtonElement = document.querySelector('#hamburger');
+const drawerElement = document.querySelector('#drawer');
+const mainElement = document.querySelector('main');
+
+hamburgerButtonElement.addEventListener('click', event => {
+  drawerElement.classList.toggle('open');
+  event.stopPropagation();
+});
+
+mainElement.addEventListener('click', event => {
+  drawerElement.classList.remove('open');
+  event.stopPropagation();
+})
