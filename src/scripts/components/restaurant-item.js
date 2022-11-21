d3238251df5155babc19afb65a20bf8d72751ec1
Yadija <ayuliadi01@gmail.com>
2022-11-21 12:46:44 +0800

    refactor: limited a desc

--- src/scripts/components/restaurant-item.js
+++ src/scripts/components/restaurant-item.js
@@ -6,6 +6,13 @@ class RestaurantItem extends HTMLElement {
     this.render();
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  showDesc(description = '-') {
+    const limit = 225;
+    if (description.length > limit) return `${description.substring(0, limit)}...`;
+    return description;
+  }
+
   render() {
     this.innerHTML = `
       <article class="card-item" tabindex="0">
@@ -16,7 +23,7 @@ class RestaurantItem extends HTMLElement {
         <div class="card-desc">
           <p class="rating">⭐️ ${this._restaurantItem.rating || '-'}</p>
           <h3 class="title"><a href="/#/detail/${this._restaurantItem.id}">${this._restaurantItem.name || '-'}</a></h3>
-          <p class="desc">${this._restaurantItem.description || '-'}</p>
+          <p class="desc">${this.showDesc(this._restaurantItem.description)}</p>
         </div>
       </article>
     `;
