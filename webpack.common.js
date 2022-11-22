97b1a1bd1cef7e9938f6f075509f91049e342324
Yadija <ayuliadi01@gmail.com>
2022-11-22 11:50:40 +0800

    feature: add bundle analyzer

--- webpack.common.js
+++ webpack.common.js
@@ -10,8 +10,8 @@ module.exports = {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
   },
   output: {
-    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
     clean: true,
   },
   module: {
